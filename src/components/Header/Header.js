457b53420293ed96980dbae3e73a51be99de50c7
xanushkasrivastavax <anushkasrivastavax@gmail.com>
2021-07-13 19:54:20 +0530

    adssd

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./style.css";
+
+export class Header extends Component {
+  render() {
+    return <header>ProInsights User- Registration</header>;
+  }
+}
+
+export default Header;
